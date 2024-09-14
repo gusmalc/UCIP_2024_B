648ce6f30b45750ebfb5d80affbe3636eb5143f6
gusmalc <doonas@doonas.net>
2024-09-13 21:05:52 -0300

    1

---
+++ clase_05/javascript/js/index.js
@@ -0,0 +1,56 @@
+let nombreUsuario = "Pedro";
+let edad = "25";
+//nombre = "Pedro";
+
+const x = 30;
+// alert(nombre);
+
+let a = "15";
+let b = 10;
+
+a = 15;
+
+let c = a+b;
+
+//console.log(c);
+
+let arr = [10,"Pedro", 25, 33, ["a","b","c"], {x: 23, y: 35}];
+
+//console.log(arr[4][0])
+
+//console.log(arr[5].x)
+
+//console.log(arr.length);
+
+    let persona = {
+        nombre: "Pedro",
+        edad: 30,
+        vive: false,
+        materias: ["Inglés","Geografía"],
+        gustos: {comer: ["Pizza","Asado"], tomar: "Birra"}
+    }
+let valor = "nombre";
+//console.log(persona.nombre);
+
+//console.log(persona[valor]);
+
+//console.log(persona.materias[0])
+
+//console.log(persona.gustos.comer[1])
+
+
+function saludo(){
+    let s = "Hola";
+    alert(s)
+}
+
+
+function suma(a,b){
+    
+    console.log(a+b)
+}
+
+
+suma(3,5);
+
+suma(20,6);
