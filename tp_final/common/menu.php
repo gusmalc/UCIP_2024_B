d38e6a4517ab326bc701bf7c0cb6e218ac7a246a
gusmalc <doonas@doonas.net>
2024-11-01 20:00:31 -0300

    1

--- tp_final/common/menu.php
+++ tp_final/common/menu.php
@@ -1,4 +1,21 @@
-<nav>
-    <a href="<?php echo $site_url; ?>">Inicio</a>
-    <a href="<?php echo $site_url . "servicios-de-entrega"; ?>">Entregas</a>
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="<?php echo $site_url; ?>">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo $site_url . "servicios-de-entrega"; ?>">Entregas</a>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
 </nav>
