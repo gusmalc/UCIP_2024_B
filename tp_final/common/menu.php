f8ec4a62fa73afa66950d287c86cad56eb05e594
gusmalc <doonas@doonas.net>
2024-11-01 19:15:18 -0300

    1

---
+++ tp_final/common/menu.php
@@ -0,0 +1,4 @@
+<nav>
+    <a href="<?php echo $site_url; ?>">Inicio</a>
+    <a href="<?php echo $site_url . "servicios-de-entrega"; ?>">Entregas</a>
+</nav>
---
+++ tp_final/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+$site_url = "http://localhost/cursos/UCIP_2024_B/tp_final/";
+//$site_url = "https://malcotti.com/";
+
+
+?>
