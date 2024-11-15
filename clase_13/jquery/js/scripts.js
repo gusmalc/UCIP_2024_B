c969d4446625d63bc2a9637688734ee6b1dd45f6
gusmalc <doonas@doonas.net>
2024-11-15 19:18:17 -0300

    1

---
+++ clase_13/jquery/js/scripts.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+
+    $( "#btn1" ).on( "click", function() {
+        alert('ok');
+        $( ".titulos" ).toggle( "color_rojo" )
+    });
+
+
+
+
+   
+});
---
+++ clase_13/webcom/js/scripts.js
@@ -0,0 +1,15 @@
+let ico_menu = document.getElementById('ico_menu');
+let menu = document.getElementById('menu');
+
+
+ico_menu.addEventListener('click', ()=>{
+    menu.classList.toggle("abrir_menu");
+})
+
+
+// el.style.cssText = "background-color: black; color: white;";
+// el.style.border = "#000 solid 2px";
+// el.classList.add("clase1", "clase2");
+// el.classList.remove("clase");
+// el.classList.toggle("clase");
+// document.getElementById("estilo").href = "css/estilosjs.css";
