5a7daa4941736863dabc3dde4c4d6c78667b9be0
gusmalc <doonas@doonas.net>
2024-11-22 19:40:36 -0300

    1

--- clase_14/formularios/js/scripts.js
+++ clase_14/formularios/js/scripts.js
@@ -5,6 +5,17 @@ let suma = document.getElementById('suma');
 btn_enviar.addEventListener('click', (e)=>{
     e.preventDefault();
     if(suma.value == "5"){
-        alert("Si envio")
+        //alert("Si envio")
+        let api_url = "enviar_email.php";
+        let data = new FormData(form_contacto);
+        //data.append("dp_sid", 1);
+
+        fetch(api_url, { method: "post", body: data })
+        .then(res => res.text())
+        .then(el => {
+            if(el == "ok")
+            //console.log(el);
+        })
+        .catch(err => { console.log(err.message) });
     }
 })
